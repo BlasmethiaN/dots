510e4df6d05d5eb053dc3adbcf43ce04f4b38020
BlasmethiaN <dominako223@gmail.com>
2022-01-31 14:09:59 +0100

    removed widgets

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -132,13 +132,13 @@ local wrap_widget = function(widget)
 end
 
 -- Wifi widget
-local create_wifi_widget = require('widgets.wifi')
-local wifi = create_wifi_widget({
-  primary = theme.color.material.green,
-  wifi_disconnected = theme.color.material.red,
-  wifi_connecting = theme.color.material.grey
-}, {icon = theme.fonts.icon, widget = theme.fonts.widget}, theme.spacing.small)
-local wifi_widget = wrap_widget(wifi.widget)
+-- local create_wifi_widget = require('widgets.wifi')
+-- local wifi = create_wifi_widget({
+--   primary = theme.color.material.green,
+--   wifi_disconnected = theme.color.material.red,
+--   wifi_connecting = theme.color.material.grey
+-- }, {icon = theme.fonts.icon, widget = theme.fonts.widget}, theme.spacing.small)
+-- local wifi_widget = wrap_widget(wifi.widget)
 
 -- Volume widget
 local volume = create_volume_widget({
@@ -157,12 +157,12 @@ local keyboard_layout = create_keyboard_layout_widget({
 local keyboard_layout_widget = wrap_widget(keyboard_layout.widget)
 
 -- Battery widget
-local create_battery_widget = require('widgets.battery')
-local battery = create_battery_widget({
-  primary = theme.color.material.cyan,
-  battery_empty = theme.color.material.red
-}, {icon = theme.fonts.icon, widget = theme.fonts.widget}, theme.spacing.small)
-local battery_widget = wrap_widget(battery.widget)
+-- local create_battery_widget = require('widgets.battery')
+-- local battery = create_battery_widget({
+--   primary = theme.color.material.cyan,
+--   battery_empty = theme.color.material.red
+-- }, {icon = theme.fonts.icon, widget = theme.fonts.widget}, theme.spacing.small)
+-- local battery_widget = wrap_widget(battery.widget)
 
 -- Calendar
 local calendaricon = wibox.widget.textbox(string.format(
