75f837da08e30f7f8e0b9a5b5e36e96ed5fcac7a
BlasmethiaN <dominako223@gmail.com>
2022-03-05 00:24:56 +0100

    💱 Changed some wallpapers

--- .config/awesome/keys.lua
+++ .config/awesome/keys.lua
@@ -221,7 +221,7 @@ awful.key {
 -- Custom keybindings
 keys.global_keys = gears.table.join(keys.global_keys, 
   awful.key({MODKEY, "Control"}, "l", function()
-  awful.spawn("sxlock")
+  awful.spawn("betterlockscreen -l")
 end, {
   description = "lock the screen",
   group = "custom"
