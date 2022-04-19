7b558b64ad393c9d99453b06a1648ad74b6f6cb5
BlasmethiaN <dominako223@gmail.com>
2022-04-19 21:41:22 +0200

    ✨ Add python check

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -9,10 +9,10 @@ lsp.bashls.setup(coq_setup {})
 lsp.ccls.setup(coq_setup {})
 lsp.cssls.setup {}
 lsp.efm.setup {
-  init_options = {documentFormatting = true},
-  filetypes = {'lua', 'sh'},
+  init_options = { documentFormatting = true },
+  filetypes = { 'lua', 'sh' },
   settings = {
-    rootMarkers = {'.git/'},
+    rootMarkers = { '.git/' },
     languages = {
       lua = {
         {
@@ -37,17 +37,17 @@ lsp.efm.setup {
 }
 -- lsp.graphql.setup {}
 lsp.hls.setup(coq_setup {
-  settings = {haskell = {formattingProvider = 'stylish-haskell'}}
+  settings = { haskell = { formattingProvider = 'ormolu' } }
 })
 
 lsp.rust_analyzer.setup {}
-
+lsp.pyright.setup(coq_setup {})
 lsp.html.setup {}
 lsp.jsonls.setup {}
 
 -- lsp.asm_lsp.setup(coq_setup {})
 
-lsp.lemminx.setup {cmd = {'/usr/bin/lemminx'}}
+lsp.lemminx.setup { cmd = { '/usr/bin/lemminx' } }
 
 local sumneko_binary_path = 'lua-language-server'
 local sumneko_root_path = '/usr/share/lua-language-server'
@@ -57,15 +57,14 @@ table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
 lsp.sumneko_lua.setup(coq_setup {
-  cmd = {sumneko_binary_path, '-E', sumneko_root_path .. '/main.lua'},
+  cmd = { sumneko_binary_path, '-E', sumneko_root_path .. '/main.lua' },
   settings = {
     Lua = {
-      runtime = {version = 'LuaJIT', path = runtime_path},
-      diagnostics = {globals = {'vim'}},
-      workspace = {library = vim.api.nvim_get_runtime_file('', true)}
+      runtime = { version = 'LuaJIT', path = runtime_path },
+      diagnostics = { globals = { 'vim' } },
+      workspace = { library = vim.api.nvim_get_runtime_file('', true) }
     }
   }
 })
 
 lsp.texlab.setup(coq_setup {})
-
